7140090dec1e71c3c6fba0239402ea97fe65f549
Thomas Weber <thomas.weber@notengrafik.com>
2022-04-18 11:48:17 +0200

    Add more tests

--- src/swing.test.ts
+++ src/swing.test.ts
@@ -20,20 +20,23 @@ function swingDocument(measures: string): [Document, () => string] {
 }
 
 test("quarter triplets", () => {
-  const [document, serialize] = swingDocument(`
+  const [document] = swingDocument(`
     <measure>
       <attributes>
         <divisions>12</divisions>
       </attributes>
+
       <note>
         <duration>12</duration>
       </note>
+
       <note>
         <duration>6</duration>
       </note>
       <note>
         <duration>6</duration>
       </note>
+
       <note>
         <duration>8</duration>
       </note>
@@ -51,4 +54,97 @@ test("quarter triplets", () => {
       .map((e) => e.textContent)
       .join(" ")
   ).toBe("36 24 12 24 24 24");
+});
+
+test("sixteenth on offbeat", () => {
+  const [document] = swingDocument(`
+    <measure>
+      <attributes>
+        <divisions>4</divisions>
+      </attributes>
+
+      <note>
+        <duration>2</duration>
+      </note>
+      <note>
+        <duration>1</duration>
+      </note>
+      <note>
+        <duration>1</duration>
+      </note>
+
+      <note>
+        <duration>2</duration>
+      </note>
+      <note>
+        <duration>2</duration>
+      </note>
+
+      <note>
+        <duration>2</duration>
+      </note>
+      <note>
+        <duration>1</duration>
+      </note>
+      <note>
+        <duration>1</duration>
+      </note>
+
+      <note>
+        <duration>4</duration>
+      </note>
+    </measure>
+  `);
+
+  expect(
+    [...document.querySelectorAll("duration")]
+      .map((e) => e.textContent)
+      .join(" ")
+  ).toBe("8 2 2 8 4 8 2 2 12");
+});
+
+test("sixteenth on downbeat", () => {
+  const [document] = swingDocument(`
+    <measure>
+      <attributes>
+        <divisions>4</divisions>
+      </attributes>
+
+      <note>
+        <duration>1</duration>
+      </note>
+      <note>
+        <duration>1</duration>
+      </note>
+      <note>
+        <duration>1</duration>
+      </note>
+      <note>
+        <duration>1</duration>
+      </note>
+
+     <note>
+        <duration>1</duration>
+      </note>
+      <note>
+        <duration>2</duration>
+      </note>
+      <note>
+        <duration>1</duration>
+      </note>
+
+      <note>
+        <duration>2</duration>
+      </note>
+      <note>
+        <duration>2</duration>
+      </note>
+    </measure>
+  `);
+
+  expect(
+    [...document.querySelectorAll("duration")]
+      .map((e) => e.textContent)
+      .join(" ")
+  ).toBe("3 3 3 3 3 6 3 8 4");
 });
